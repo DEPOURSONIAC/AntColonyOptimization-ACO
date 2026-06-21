a43e94c391a1bc8efcd5bee9c69284ce2ae9cb33
DEPOUROSNIAC <DEPOURSONIAC.JEAN@outlook.com>
2026-06-21 17:48:07 +0200

    Add files via upload

---
+++ moduleAco.py
@@ -0,0 +1,83 @@
+# coding: utf-8
+
+
+"""
+Module ACO (Ant Colony Optimization)
+
+Ce module contient l'algo de colonie de fourmis.
+Il gère le meilleur chemin entre les villes:
+    - il gère les probas via les phéromones
+    - la dist entre les villes
+    - l'évaporation  et le dépot des phéromones
+
+"""
+import random
+import numpy as np
+import tools
+import time
+
+# Décla des constantes (en MAJUSCULES)
+
+ALPHA       : float = 1.0
+BETA        : float = 1.0
+EVAPORATION : float = 0.5
+Q           : float = 100 # Ratio du nombre de phéromone sur une route/ chemin
+
+
+# fonctions
+def ACO(nombreVilles: int = 100, nombreFourmis: int = 100, nombreIterations: int = 50):
+    """
+    ACO (Ant Colony Optimization)
+
+    Paramètres :
+        - nombreVilles (int)     : nombre total de villes du problème
+        - nombreFourmis (int)    : nombre de fourmis utilisées à chaque itération
+        - nombreIterations (int) : nombre de cycles d’exécution de l’algorithme
+
+    Explication :
+        - implémente l’algo du ACO
+        - chaque fourmi construit un chemin complet entre les villes
+        - les choix sont crées par les phéromones et les distances
+        - les bonnes solutions renforcent les chemins via le dépôt de phéromones
+        - les mauvaises solutions s’affaiblissent avec l’évaporation
+
+    Retour :
+        - tuple :
+            - le meilleur chemin trouvé
+            - la distance minimale associée
+            - le temps mis pour le trouver
+    """
+
+    # Décla des variables
+    debut = time.perf_counter()
+
+    meilleurChemin   = None
+    meilleurDistance = np.inf
+
+    villes   = tools.creationDesVilles(nombreVilles)
+    distances = tools.matriceDistanceEuclidienne(villes, nombreVilles)
+
+    # Matrice des phéromones : toutes les valeurs sont mises à 1. Elle sert à aider les fourmis dans le choix des chemins
+    MatricePheromones : np.ndarray = np.ones((nombreVilles, nombreVilles))
+
+    # Algo du ACO
+    for iteration in range(nombreIterations):
+
+        # Liste des chemins trouvés par les fourmis
+        cheminTrouverParFourmis : list = list()
+        # Liste des distances associées
+        listeDistance           : list = list()
+
+        
+
+        print(' TEST ITERATION')
+   
+    # Mesurer le temps que ça prends
+    fin = time.perf_counter()
+    deltaT = fin - debut
+
+    return (meilleurChemin, meilleurDistance, deltaT )
+
+
+if ( __name__ == "__main__"):
+    print('HelloWorld')
